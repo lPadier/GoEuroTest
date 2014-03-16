2d4ece0533fdc8d3dc69e5e33c188775249088c7
Loïc Padier <loic@Loics-MacBook-Air.local>
2014-03-17 00:51:45 +0100

    completed code

--- GoEuroTest.java
+++ GoEuroTest.java
@@ -1,32 +1,28 @@
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.lang.RuntimeException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Scanner;
-import java.io.FileOutputStream;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.FileNotFoundException;
-import java.lang.RuntimeException;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.JSONArray;
 import org.json.simple.parser.ParseException;
 
 
 public class GoEuroTest {
 	public static JSONObject getJSON(String addr) {
-		// String s = "https://api.goeuro.com/api/v1/suggest/position/en/name/";
-		String s = "http://192.168.0.16/";
+		String s = "https://api.goeuro.com/api/v1/suggest/position/en/name/";
 		try {
 			s += URLEncoder.encode(addr, "UTF-8");
-			System.out.println(s);
 			URL url = new URL(s);
 			Scanner scan = new Scanner(url.openStream());
-  
-
 			String str = new String();
 			while (scan.hasNext()) {
 				str += scan.nextLine();
@@ -36,13 +32,24 @@ public class GoEuroTest {
 			Object obj=parser.parse(str);
 			return (JSONObject) obj;
 		}
-		catch (ParseException e)				{System.out.println("PE " + e);}
-		catch (UnsupportedEncodingException e)	{System.out.println("UEE");}
-		catch (MalformedURLException e) 		{System.out.println("MUE");}
-		catch (IOException e) 					{System.out.println("IOE" + e);}
-		throw new RuntimeException();
+		catch (ParseException e)				{System.out.println("ParseException : " + e);}
+		catch (UnsupportedEncodingException e)	{System.out.println("UnsupportedEncodingException : " + e);}
+		catch (MalformedURLException e) 		{System.out.println("MalformedURLException : " + e);}
+		catch (IOException e) 					{System.out.println("IOException : " + e);}
+		throw new RuntimeException("Error while getting JSON");
 	}
 	
+	public static String jsonGet(String name, JSONObject resultJsonObject, Class<?> cls) {
+		Object result=resultJsonObject.get(name);
+		if (cls == String.class) {
+			return (result instanceof String) ? "\""+(String) result+"\"" : "null";
+		} else if (cls == Number.class) {
+			return (result instanceof Number) ? String.valueOf(result) : "null";
+		} else {
+			return null;
+		}
+	}
+
 	public static void writeCSV (JSONObject jsonObject) {
 		if (jsonObject.get("results") instanceof JSONArray) {
 			
@@ -55,40 +62,39 @@ public class GoEuroTest {
 					if (resultObject instanceof JSONObject) {
 						writer.newLine();
 						JSONObject resultJsonObject = (JSONObject) resultObject;
-						String _type, id, name, type, latitude, longitude;
+						String _type, _id, name, type, latitude, longitude;
 						
-						_type = (resultJsonObject.get("_type") instanceof String) ? "\""+(String) resultJsonObject.get("_type")+"\"" : "null";
-						id = (resultJsonObject.get("_id") instanceof Number) ? String.valueOf( resultJsonObject.get("_id")) : "null";
-						name = (resultJsonObject.get("name") instanceof String) ? "\""+(String) resultJsonObject.get("name")+"\"" : "null";
-						type = (resultJsonObject.get("type") instanceof String) ? "\""+(String) resultJsonObject.get("type")+"\"" : "null";
+						_type = jsonGet("_type",resultJsonObject,String.class);
+						_id   = jsonGet("_id",resultJsonObject,Number.class);
+						name  = jsonGet("name",resultJsonObject,String.class);
+						type  = jsonGet("type",resultJsonObject,String.class);
 
 						if (resultJsonObject.get("geo_position") instanceof JSONObject) {
 							JSONObject geoPosition = (JSONObject) resultJsonObject.get("geo_position");
-							if ( geoPosition.get("latitude") instanceof Number) {
-								latitude = String.valueOf( geoPosition.get("latitude"));
-								longitude = String.valueOf( geoPosition.get("longitude"));
-							}
-							else {
+							latitude  = jsonGet("latitude",geoPosition,Number.class);
+							longitude = jsonGet("longitude",geoPosition,Number.class);
+						} else {
 								latitude="null";
 								longitude="null";
 							}
-						}else {
-								latitude="null";
-								longitude="null";
-							}
-					
-						writer.write(_type + " , " + id + " , " + name + " , " + type + " , " + latitude + " , " + longitude);
+						writer.write(_type + " , " + _id + " , " + name + " , " + type + " , " + latitude + " , " + longitude);
 					}
 				}
 				writer.close();
+				System.out.println("Contents written to output.csv");
 			}
 			catch (FileNotFoundException e) 		{System.out.println("FNFE" + e);}
 			catch (UnsupportedEncodingException e)	{System.out.println("UEE");}
 			catch (MalformedURLException e) 		{System.out.println("MUE");}
 			catch (IOException e) 					{System.out.println("IOE" + e);}
-		}
-		else { //results is empty
-			System.out.println("There are no results");
+		} else {
+			if (jsonObject.get("results") == null) {
+				// results is emply
+				System.out.println("There are no results");
+			} else {
+				// results is neither emply nor an array
+				System.out.println("results is neither empty nor an array");
+			}
 		}
 	}
 
@@ -99,7 +105,7 @@ public class GoEuroTest {
 				writeCSV(jsonObject);
 			}
 			catch (RuntimeException e) {
-				System.out.println("Error while getting JSON." + e);
+				System.out.println(e);
 				return;
 			}
 		} else {
